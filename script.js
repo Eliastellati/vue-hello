a5f71b342dc1bce03d3b3aabd465496bd53e7d27
eliastellati <eliastellatibvb@gmail.com>
2021-05-28 16:41:41 +0200

    esercizio completato piu bonus

---
+++ script.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: "#root",
+    data: {
+        message: "Stampo un messaggio usando Vue.js :)",
+
+        immagine: 'https://vue.nodegui.org/img/undraw_monitor.png'
+    }
+})
